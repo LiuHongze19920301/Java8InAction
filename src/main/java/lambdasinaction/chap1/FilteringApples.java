8868ec46487647e4a0b9692e1888e6af1fd788b7
liuhongze <lhz920301@163.com>
2019-07-22 18:41:17 +0800

    Add Some Changes

--- src/main/java/lambdasinaction/chap1/FilteringApples.java
+++ src/main/java/lambdasinaction/chap1/FilteringApples.java
@@ -1,81 +1,96 @@
 package lambdasinaction.chap1;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
 
-public class FilteringApples{
+public class FilteringApples {
 
-    public static void main(String ... args){
+    private static final Predicate<Apple> GREEN_APPLE_PREDICATE = FilteringApples::isGreenApple;
+    private static final Predicate<Apple> HEAVY_APPLE_PREDICATE = FilteringApples::isHeavyApple;
+    private static final Predicate<Plant> HIGH_PLANT_PREDICATE = FilteringApples::isHighPlant;
 
-        List<Apple> inventory = Arrays.asList(new Apple(80,"green"),
-                                              new Apple(155, "green"),
-                                              new Apple(120, "red"));	
+    public static void main(String... args) {
 
-        // [Apple{color='green', weight=80}, Apple{color='green', weight=155}]
+        List<Apple> inventory = Arrays.asList(
+                new Apple(80, "green", 0.8),
+                new Apple(155, "green", 0.7),
+                new Apple(120, "red", 0.6),
+                new Apple(180, "brown", 0.5)
+        );
+
+        // [Apple{color='green', weight=80, height=0.8}, Apple{color='green', weight=155, height=0.7}]
         List<Apple> greenApples = filterApples(inventory, FilteringApples::isGreenApple);
         System.out.println(greenApples);
-        
-        // [Apple{color='green', weight=155}]
+
+        // [Apple{color='green', weight=155, height=0.7}, Apple{color='brown', weight=180, height=0.5}]
         List<Apple> heavyApples = filterApples(inventory, FilteringApples::isHeavyApple);
         System.out.println(heavyApples);
-        
-        // [Apple{color='green', weight=80}, Apple{color='green', weight=155}]
-        List<Apple> greenApples2 = filterApples(inventory, (Apple a) -> "green".equals(a.getColor()));
+
+        // [Apple{color='green', weight=80, height=0.8}, Apple{color='green', weight=155, height=0.7}]
+        List<Apple> greenApples2 = filterApples(inventory, apple -> "green".equals(apple.getColor()));
         System.out.println(greenApples2);
-        
-        // [Apple{color='green', weight=155}]
-        List<Apple> heavyApples2 = filterApples(inventory, (Apple a) -> a.getWeight() > 150);
+
+        // [Apple{color='green', weight=155, height=0.7}, Apple{color='brown', weight=180, height=0.5}]
+        List<Apple> heavyApples2 = filterApples(inventory, apple -> apple.getWeight() > 150);
         System.out.println(heavyApples2);
-        
-        // []
-        List<Apple> weirdApples = filterApples(inventory, (Apple a) -> a.getWeight() < 80 || 
-                                                                       "brown".equals(a.getColor()));
+
+        // [Apple{color='brown', weight=180, height=0.5}]
+        List<Apple> weirdApples = filterApples(inventory, apple -> apple.getWeight() < 80 ||
+                "brown".equals(apple.getColor()));
         System.out.println(weirdApples);
+
+        // [Apple{color='green', weight=80, height=0.8}, Apple{color='green', weight=155, height=0.7}, Apple{color='brown', weight=180, height=0.5}]
+        Predicate<Apple> greenAndHeavyApplePredicate = ((Predicate<Apple>) FilteringApples::isGreenApple)
+                .or((Predicate<? super Apple>) FilteringApples::isHeavyApple);
+        List<Apple> greenOrHeavyApple = filterApples(inventory, greenAndHeavyApplePredicate);
+        System.out.println(greenOrHeavyApple);
+
+        // [Apple{color='green', weight=80, height=0.8}, Apple{color='green', weight=155, height=0.7}]
+        List<Apple> highPlants = filterApples(inventory, HIGH_PLANT_PREDICATE);
+        System.out.println(highPlants);
+
     }
 
-    public static List<Apple> filterGreenApples(List<Apple> inventory){
-        List<Apple> result = new ArrayList<>();
-        for (Apple apple: inventory){
-            if ("green".equals(apple.getColor())) {
-                result.add(apple);
-            }
-        }
-        return result;
-    }
-
-    public static List<Apple> filterHeavyApples(List<Apple> inventory){
-        List<Apple> result = new ArrayList<>();
-        for (Apple apple: inventory){
-            if (apple.getWeight() > 150) {
-                result.add(apple);
-            }
-        }
-        return result;
+    public static boolean isHighPlant(Plant plant) {
+        return plant.height > 0.6;
     }
 
     public static boolean isGreenApple(Apple apple) {
-        return "green".equals(apple.getColor()); 
+        return "green".equals(apple.getColor());
     }
 
     public static boolean isHeavyApple(Apple apple) {
         return apple.getWeight() > 150;
     }
 
-    public static List<Apple> filterApples(List<Apple> inventory, Predicate<Apple> p){
+    public static List<Apple> filterApples(List<Apple> inventory, Predicate<? super Apple> p) {
         List<Apple> result = new ArrayList<>();
-        for(Apple apple : inventory){
-            if(p.test(apple)){
+        for (Apple apple : inventory) {
+            if (p.test(apple)) {
                 result.add(apple);
             }
         }
         return result;
-    }       
+    }
 
-    public static class Apple {
-        private int weight = 0;
-        private String color = "";
+    public static class Apple extends Plant {
+        private int weight;
+        private String color;
 
-        public Apple(int weight, String color){
+        public Apple() {
+            super();
+        }
+
+        public Apple(int weight, String color) {
+            super();
+            this.weight = weight;
+            this.color = color;
+        }
+
+        public Apple(int weight, String color, double height) {
+            super(height);
             this.weight = weight;
             this.color = color;
         }
@@ -98,9 +113,29 @@ public class FilteringApples{
 
         public String toString() {
             return "Apple{" +
-                   "color='" + color + '\'' +
-                   ", weight=" + weight +
-                   '}';
+                    "color='" + color + '\'' +
+                    ", weight=" + weight +
+                    ", height=" + getHeight() +
+                    '}';
+        }
+    }
+
+    public static abstract class Plant {
+        private double height;
+
+        public Plant() {
+        }
+
+        public Plant(double height) {
+            this.height = height;
+        }
+
+        public double getHeight() {
+            return height;
+        }
+
+        public void setHeight(double height) {
+            this.height = height;
         }
     }
 
