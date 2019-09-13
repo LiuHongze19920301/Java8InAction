145c0e54323fd7e3d8b4a0f1feb84dc9b405c199
liuhongze <lhz920301@163.com>
2019-09-13 18:03:57 +0800

    Enhance FilteringApples

--- src/main/java/lambdasinaction/chap1/FilteringApples.java
+++ src/main/java/lambdasinaction/chap1/FilteringApples.java
@@ -1,14 +1,24 @@
 package lambdasinaction.chap1;
 
 import java.util.*;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class FilteringApples {
 
-    private static final Predicate<Apple> GREEN_APPLE_PREDICATE = FilteringApples::isGreenApple;
+    private static final Predicate<Apple> GREEN_APPLE_PREDICATE = FilteringApples::isHeavyApple;
     private static final Predicate<Apple> HEAVY_APPLE_PREDICATE = FilteringApples::isHeavyApple;
+    private static final Consumer<Apple> PRINT_APPLE_CONSUMER_V1 = FilteringApples::printApple;
+    private static final Consumer<Apple> PRINT_APPLE_CONSUMER_V2 = FilteringApples::printAppleTwice;
     private static final Predicate<Plant> HIGH_PLANT_PREDICATE = FilteringApples::isHighPlant;
+
+    private static final Map<Predicate<Apple>, Consumer<Apple>> APPLE_OPERATION_MAP = new HashMap<>();
+
+    static {
+        APPLE_OPERATION_MAP.put(GREEN_APPLE_PREDICATE, PRINT_APPLE_CONSUMER_V1);
+        APPLE_OPERATION_MAP.put(HEAVY_APPLE_PREDICATE, PRINT_APPLE_CONSUMER_V2);
+    }
 
     public static void main(String... args) {
 
@@ -62,6 +72,14 @@ public class FilteringApples {
         List<Apple> highPlants = filterApples(inventory, HIGH_PLANT_PREDICATE);
         System.out.println(highPlants);
 
+        System.out.println("***********************");
+        Apple myTestApple = new Apple(180, "green", 0.8);
+        APPLE_OPERATION_MAP.forEach((key, value) -> {
+            if (key.test(myTestApple)) {
+                value.accept(myTestApple);
+            }
+        });
+
     }
 
     private static boolean isHighPlant(Plant plant) {
@@ -76,7 +94,20 @@ public class FilteringApples {
         return apple.getWeight() > 150;
     }
 
+    private static void printApple(Apple apple) {
+        System.out.println(apple);
+    }
+
+    private static void printAppleTwice(Apple apple) {
+        System.out.println(apple);
+        System.out.println(apple);
+    }
+
     private static List<Apple> filterApples(List<Apple> inventory, Predicate<? super Apple> p) {
+        Objects.requireNonNull(p, "No Predicate Specified!");
+        if (Objects.isNull(inventory)) {
+            return null;
+        }
         List<Apple> result = new ArrayList<>();
         for (Apple apple : inventory) {
             if (p.test(apple)) {
@@ -87,6 +118,7 @@ public class FilteringApples {
     }
 
     private static <T> List<T> filterTypes(List<T> types, Predicate<? super T> predicate) {
+        Objects.requireNonNull(predicate, "No Predicate Specified!");
         if (Objects.isNull(types)) {
             return null;
         }
@@ -103,6 +135,7 @@ public class FilteringApples {
     }
 
     private static <T> List<T> filterTypesWithStream(List<T> types, Predicate<? super T> predicate) {
+        Objects.requireNonNull(predicate, "No Predicate Specified!");
         if (Objects.isNull(types)) {
             return null;
         }
